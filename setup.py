0b94fe510c016940308e9039825f10eeb0d0d532
edison <y.ge1222@gmail.com>
2019-09-16 15:10:28 +0200

    add dependancy

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/uqyge/ODENet",
     packages=setuptools.find_packages(),
+    install_requires=["pyvista", "matplotlib"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
